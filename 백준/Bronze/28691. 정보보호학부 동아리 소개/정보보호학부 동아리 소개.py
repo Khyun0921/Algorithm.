ce553750de5c5e3ba4742adecb3604e4a548d5b1
Khyun0921 <151927618+Khyun0921@users.noreply.github.com>
2024-01-05 16:18:31 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,11 @@
+A = input()
+if A == "M":
+    print("MatKor")
+elif A == "W":
+    print("WiCys")
+elif A == "C":
+    print("CyKor")
+elif A == "A":
+    print("AlKor")
+elif A == "$":
+    print("$clear")
